1538efe2b49567dc904a37b132b07c69747e67d8
amir-hossain113 <71897803+amir-hossain113@users.noreply.github.com>
2022-01-02 00:17:13 -0800

    Add files via upload

---
+++ Rotating_Fan_Project/main.cpp
@@ -0,0 +1,97 @@
+#include<windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <math.h>
+
+static GLfloat spin = 0.0;
+
+
+void Display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+
+	glColor3f(1.0, 1.0, 1.0);
+
+
+	glRectf(-25.0, -4.0, 25.0, 4.0);
+	glRectf(-4.0, -25.0, 4.0, 25.0);
+	glColor3f(0.0, 1.0, 1.0);
+	glutSolidTorus(1.00, 6.4, 10, 100);
+	glPopMatrix();
+
+	glFlush();
+}
+
+
+void Spin_Display_Right1(void)
+{
+   spin = -.60;
+			glutPostRedisplay();
+			glRotatef(spin, 0.0, 0.0, 1.0);
+}
+
+
+void Init(void)
+{
+	glClearColor (0.0, 1.0, 0.0, 0.0);
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+}
+
+int a = 0;
+void My_Keyboard(unsigned char key, int x, int y)
+{
+
+	switch (key)
+	{
+
+		case 's':
+		    {
+                if(a == 0)
+                    {
+                        glutIdleFunc(Spin_Display_Right1);
+                        a = 1;
+                    }
+                else
+                    {
+                        glutIdleFunc(NULL);
+                        a = 0;
+                    }
+            }
+
+        case 'h':
+			{
+                glRotatef(-10, 0.0, 0.0, 1.0);
+                break;
+			}
+
+        case 'l':
+            {
+                glRotatef(3, 0.0, 0.0, 1.0);
+                break;
+            }
+
+    default:
+    break;
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (1000, 600);
+	glutInitWindowPosition (100, 100);
+
+	glutCreateWindow ("Rotating Fan");
+	Init();
+
+
+	glutDisplayFunc(Display);
+	glutKeyboardFunc(My_Keyboard);
+
+	glutMainLoop();
+	return 0;
+}
